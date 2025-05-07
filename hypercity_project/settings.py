00018dc145294709c2b27facfbf6f0e931da370f
DESKTOP-HEIP9LK\user <muhammedashique8281@gmail.com>
2025-05-07 18:36:59 +0530

    first commit

--- hypercity_project/settings.py
+++ hypercity_project/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app1.middleware.AuthRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'hypercity_project.urls'
